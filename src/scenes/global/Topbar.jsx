540145abb6b5a003b96f699d46496442e1fabfed
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-22 23:53:13 +0200

    set Search input in header of Box

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,7 @@
 import { Box, IconButton, useTheme } from "@mui/material"
 import { useContext } from "react"
 import { ColorModeContext, token, tokens } from "../../theme"
-import InputBase from "@mui/material"
+import {InputBase} from "@mui/material"
 import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
 import  DarkModeOutlinedIcon  from "@mui/icons-material/DarkModeOutlined"
 import  NotificationsOutlinedIcon  from "@mui/icons-material/NotificationsOutlined"
@@ -24,7 +24,7 @@ const Topbar = () => {
       backgroundColor={colors.primary[400]}
       borderRadius="3px"
       >
-        
+        <InputBase sx={{ml:2, flex:1}} placeholder="Search"/>
       </Box>
     <IconButton sx={{display:"flex"}}></IconButton>
   </Box>
