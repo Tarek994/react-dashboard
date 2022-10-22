d21289a82e27d320e263e08e5c28f608e9558923
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-22 23:47:25 +0200

    set header box

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -18,8 +18,14 @@ const Topbar = () => {
 
 
   return <Box display= "flex" justifyContent="space-between" p={2}>
-
-
+    {/* SEARCH BAR */}
+      <Box
+      display="flex"
+      backgroundColor={colors.primary[400]}
+      borderRadius="3px"
+      >
+        
+      </Box>
     <IconButton sx={{display:"flex"}}></IconButton>
   </Box>
     
