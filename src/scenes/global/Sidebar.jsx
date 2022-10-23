61b79ea172291c4eefcc3a889945332ed0cda293
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-23 03:46:22 +0200

    set ProSidebar fanction

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -45,6 +45,39 @@ const Sidebar = () => {
         }
       }}
     >
+      <ProSidebar collapsed={isCollapsed}>
+          <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+          onClick={() => setIsCollapsed(!isCollapsed)}
+          icon={isCollapsed? <MenuOutlinedIcon/> : undefined}
+          style={{
+            margin:"10px 0 20px 0",
+            color: colors.gray[100],
+          }}
+          >
+            {isCollapsed &&(
+              <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              ml="15px"
+              >
+                <Typography variant="h3" color={colors.gray[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(isCollapsed)}>
+                <MenuOutlinedIcon/>
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+            
+            
+
+          </Menu>
+      </ProSidebar>
+
 
     </Box>
     
