b2062513695113eb8b8b7c62016bbfb0b47638af
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-23 22:37:00 +0200

    fix sidebar version

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
-
 import "react-pro-sidebar/dist/css/styles.css";
 import { Box, IconButton, Typography,useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
+
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
@@ -46,7 +46,7 @@ const Sidebar = () => {
         }
       }}
     >
-      <ProSidebar collapsed={isCollapsed}>
+       <ProSidebar collapsed={isCollapsed}>
           <Menu iconShape="square">
           {/* LOGO AND MENU ICON */}
           <MenuItem
