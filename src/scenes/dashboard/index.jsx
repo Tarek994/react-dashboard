ea01d7a0ad297a6bd71d12f1e3d51348bc6fc960
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-25 23:49:04 +0200

    style box for title of header

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,12 +1,19 @@
+import { Box } from "@mui/system";
 import Header from "../../components/Header";
 
 
 
 const Dashboard = () => {
   return (
-    <div>
-      <Header/>
-      </div>
+    <Box m="20px">
+      <Box
+       display="flex"
+       justifyContent="space-between"
+       alignItems="center"
+       >
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard"/>
+      </Box>
+      </Box>
   )
 }
 
