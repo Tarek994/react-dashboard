02acc4b2ff8370cea2911edcaa646dedb2de2ea6
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-25 23:32:12 +0200

    set first Box of Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,22 @@ const Header = ({title, subtitle}) =>{
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     return(
-        <Box>
-            <Typography>{title}</Typography>
+        <Box mb="30px">
+            <Typography
+             variant="h2"
+              color={colors.gray[100]}
+              fontWeight="bold"
+              sx={{ mb : "5px"}}
 
+            >
+            {title}</Typography>
+
+            <Typography
+            variant="h5"
+            color={colors.greenAccent[400]}
+            ></Typography>
         </Box>
-
-    )
-
-}
+    );
+};
 
 export default Header;
