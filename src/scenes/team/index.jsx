1202b4b1a5e4b42e27681cef2186038e67588006
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-26 00:49:01 +0200

    import most important icons for mannge team section

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -1,3 +1,12 @@
+import  {Box, Typography, useTheme} from "@mui/material";
+import  { DataGrid } from "@mui/x-data-grid";
+import  tokens from "../../theme";
+import  {mocDataTeam} from "../../data/mocData";
+import  AdminPanelSettingsOutlinedIcon  from "@mui/icons-material/AdminPanelSettingsOutlined";
+import  LockOpenOutlinedIcon  from "@mui/icons-material/LockOpenOutlined";
+import  SecurityOutlinedIcon  from "@mui/icons-material/SecurityOutlined";
+import  Header from "../../components/Header"
+
 
 const Team = () => {
   return (
