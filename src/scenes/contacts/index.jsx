d486b30d7143edf020fba66613ae4327d551e982
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-26 23:39:35 +0200

    Fix name of Contacts component

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -5,7 +5,7 @@ import  {mockDataContacts} from "../../data/mocData";
 import  Header from "../../components/Header"
 
 
-const Team = () => {
+const Contacts = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
@@ -83,4 +83,4 @@ const Team = () => {
   )
 }
 
-export default Team;
+export default Contacts;
