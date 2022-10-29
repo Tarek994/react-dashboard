a6bffa8f7e64058f1a913ca6383441291972600b
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-29 23:32:12 +0200

    add second textField

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -71,6 +71,20 @@ const Form = () =>{
                     sx={{gridColumn: " span 2" }}
 
                     />
+                        <TextField 
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="First Name"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.firstName}
+                    name="firstName"
+                    error={!!touched.firstName && !!errors.firstName}
+                    helperText={touched.firstName && errors.firstName}
+                    sx={{gridColumn: " span 2" }}
+
+                    />
 
 
                     </Box>
