ec093670fc3e1e3ed3e950b151eb5a354b2143b4
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-29 23:42:38 +0200

    chore(app-root)

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -50,8 +50,8 @@ const Form = () =>{
          >
             {({values, errors, touched, handleBlur, handleChange, handleSubmit}) =>(
 
-                 <form onSubmit={handleSubmit}>
-                    <Box display="grid" gap="30px"
+              <form onSubmit={handleSubmit}>
+                <Box display="grid" gap="30px"
                      gridTemplateColumns="repeat(4, minmax(0, 1fr))"
                      sx={{
                         "& > div ": {gridColumn : isNonMobile ? undefined : "span 4 "}
@@ -139,33 +139,12 @@ const Form = () =>{
                     error={!!touched.address2 && !!errors.address2}
                     helperText={touched.address2 && errors.address2}
                     sx={{gridColumn: " span 4" }}
-
                     />
-                        <TextField 
-                    fullWidth
-                    variant="filled"
-                    type="text"
-                    label="First Name"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.firstName}
-                    name="firstName"
-                    error={!!touched.firstName && !!errors.firstName}
-                    helperText={touched.firstName && errors.firstName}
-                    sx={{gridColumn: " span 2" }}
-
-                    />
-
-
-                    </Box>
-                    
-                </form>
-                 
-                 )} 
-
-            
+                </Box>
+               </form>
+            )} 
             </Formik>        
-    </Box>
+         </Box>
 
     )
   
