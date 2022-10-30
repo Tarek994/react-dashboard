7316c0597c6110232fecb859a48a26b80bd84c87
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-30 03:46:25 +0100

    fix bug

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -41,7 +41,7 @@ const Form = () =>{
     return(
     <Box m="20px">
         <Header title="CREATE USER"
-         subtitle="Create a new user Profile"/>
+         subtitle="Create a New User Profile"/>
                 
          <Formik
             onSubmit={handleFormSubmit}
@@ -54,22 +54,21 @@ const Form = () =>{
                 <Box display="grid" gap="30px"
                      gridTemplateColumns="repeat(4, minmax(0, 1fr))"
                      sx={{
-                        "& > div ": {gridColumn : isNonMobile ? undefined : "span 4 "}
+                        "& > div ": {gridColumn : isNonMobile ? undefined : "span 4 "},
                      }} 
                      >
-                    <TextField 
-                    fullWidth
-                    variant="filled"
-                    type="text"
-                    label="First Name"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.firstName}
-                    name="firstName"
-                    error={!!touched.firstName && !!errors.firstName}
-                    helperText={touched.firstName && errors.firstName}
-                    sx={{gridColumn: " span 2" }}
-
+                    <TextField
+                        fullWidth
+                        variant="filled"
+                        type="text"
+                        label="First Name"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.firstName}
+                        name="firstName"
+                        error={!!touched.firstName && !!errors.firstName}
+                        helperText={touched.firstName && errors.firstName}
+                        sx={{ gridColumn: "span 2" }}
                     />
                     <TextField
                         fullWidth
