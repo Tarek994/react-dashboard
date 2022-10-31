1b02d7f159bfe49bfa1911d6ce23b3955fa60f6c
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-01 00:06:12 +0200

    import Header and other files to create calendar page

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -4,3 +4,24 @@ import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
 import { InteractionPlugin } from "@fullcalendar/interaction"
 import listPlugin from "@fullcalendar/list"
+import{
+ Box,
+ List,
+ ListItem,
+ ListItemText,
+ Typography,
+ useTheme,
+}from " @mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme"
+
+
+const Calendar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [currentEvents, setCurrentEvents] = useState([]);
+    const handleDateClick = (selected) => {
+        const title = prompt("Pleas enter a new title for your event")
+    }
+}
+export default Calendar;
