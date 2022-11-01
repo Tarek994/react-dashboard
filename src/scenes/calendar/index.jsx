a084e0a4a65a2066ffb92c6ca7be6c3bfbe2061f
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-01 03:15:57 +0200

    creat(calendar): add calendar section

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -2,11 +2,9 @@ import  {useState} from "react"
 import FullCalendar, {formatDate} from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
-import { InteractionPlugin } from "@fullcalendar/interaction"
+import interactionPlugin  from "@fullcalendar/interaction"
 import listPlugin from "@fullcalendar/list"
-
 import { Box,List, ListItem, ListItemText,Typography,useTheme } from "@mui/material"
-
 import Header from "../../components/Header";
 import { tokens } from "../../theme"
 
@@ -23,9 +21,9 @@ const Calendar = () => {
 
         if(title) {
              calendarApi.addEvent({
-                id: `${selected.dateStr} -${title}`,
+                id: `${selected.dateStr}-${title}`,
                 title,
-                start:selected.startStrr,
+                start: selected.startStr,
                 end: selected.endStr,
                 allDay: selected.allDay
              });
@@ -47,16 +45,19 @@ const Calendar = () => {
             <Box
              flex="1 1 20%" 
              backgroundColor={colors.primary[400]}
-             p="15px" borderRadius="4px"
+             p="15px"
+              borderRadius="4px"
              >
+            
               <Typography variant="h5">Events</Typography>
               <List>
-                {currentEvents.map((event)=>(
+                {currentEvents.map((event)=> (
                 <ListItem
                     key={event.id}
-                    sx={{backgroundColor: colors.greenAccent[500],
-                    margin: "10px 0 ",
-                    borderRadius: "2px"
+                    sx={{
+                        backgroundColor: colors.greenAccent[500],
+                        margin: "10px 0px",
+                        borderRadius: "2px",
                 }}>
                     <ListItemText
                       primary={event.title}
@@ -74,8 +75,46 @@ const Calendar = () => {
                 ))}
               </List>
             </Box>
-          </Box>
+            
+            {/* CALENDAR */}
+            <Box flex="1 1 100%" ml="15px">
+            <FullCalendar
+                height="75vh"
+                plugins={[
+                dayGridPlugin,
+                timeGridPlugin,
+                interactionPlugin,
+                listPlugin,
+                ]}
+                headerToolbar={{
+                left: "prev,next today",
+                center: "title",
+                right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+                }}
+                initialView="dayGridMonth"
+                editable={true}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={true}
+                select={handleDateClick}
+                eventClick={handleEventClick}
+                eventsSet={(events) => setCurrentEvents(events)}
+                initialEvents={[
+                {
+                    id: "12315",
+                    title: "All-day event",
+                    date: "2022-09-14",
+                },
+                {
+                    id: "5123",
+                    title: "Timed event",
+                    date: "2022-09-28",
+                },
+                ]}
+            />
+            </Box>
         </Box>
-    )
-};
+        </Box>
+    );
+    };
 export default Calendar;
