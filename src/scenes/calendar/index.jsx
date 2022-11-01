a42262e050d779dd80a78e17851e69160c6ba0e7
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-01 03:18:43 +0200

    fix(calendar): fix bug 'dispaly/display'

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -38,43 +38,43 @@ const Calendar = () => {
 
     return(
         <Box m="20px">
-           <Header title="CALENDAR" subtitle="Full Calendar Interactiv Page"/> 
+      <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
 
-          <Box dispaly="flex" justifyContent="space-between">
-            {/* CALENDAR SIDEBAR */}
-            <Box
-             flex="1 1 20%" 
-             backgroundColor={colors.primary[400]}
-             p="15px"
-              borderRadius="4px"
-             >
-            
-              <Typography variant="h5">Events</Typography>
-              <List>
-                {currentEvents.map((event)=> (
-                <ListItem
-                    key={event.id}
-                    sx={{
-                        backgroundColor: colors.greenAccent[500],
-                        margin: "10px 0px",
-                        borderRadius: "2px",
-                }}>
-                    <ListItemText
-                      primary={event.title}
-                      secondary={
-                        <Typography>
-                            {formatDate(event.start, {
-                                year: "numeric",
-                                month: "short",
-                                day: "numeric"
-                            })}
-                        </Typography>
-                        }
-                    />
-                </ListItem>
-                ))}
-              </List>
-            </Box>
+      <Box display="flex" justifyContent="space-between">
+        {/* CALENDAR SIDEBAR */}
+        <Box
+          flex="1 1 20%"
+          backgroundColor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
             
             {/* CALENDAR */}
             <Box flex="1 1 100%" ml="15px">
