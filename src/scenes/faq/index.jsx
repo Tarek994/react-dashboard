16f9063e42eea68216a1cbc5fbb001f31b657bde
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-03 00:23:28 +0200

    setup: set up the development environment

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,9 +1,22 @@
+import { Box,useTheme,Typography } from "@mui/material"
+import Header from "../../components/Header"
+import Accordion from "@mui/material/Accordion"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import  ExpandMoreIcon  from "@mui/icons-material/ExpandMore"
+import { tokens } from "../../theme"
+
+const FAQ = () => {
+     
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
 
 
-const index = () => {
   return (
-    <div>index</div>
+    <Box m="20px">
+
+    </Box>
   )
 }
 
-export default index
+export default FAQ;
