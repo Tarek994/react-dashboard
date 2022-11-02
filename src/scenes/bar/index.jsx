d3af7094edfe19e9e308aa907d6ae8041149f3ae
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-03 01:59:38 +0200

    Bar: create Bar component

---
+++ src/scenes/bar/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/system";
+import Header from "../../components/Header";
+import BarChart from "../../components/BarChart"
+
+
+const Bar = () => {
+  return (
+    <div>Bar</div>
+  )
+}
+
+export default Bar
