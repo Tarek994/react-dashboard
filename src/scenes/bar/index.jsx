2acb3397327aaedb4e3acccf7b11c139584458f9
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-03 02:02:35 +0200

    Bar: set the Bar content

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -5,8 +5,13 @@ import BarChart from "../../components/BarChart"
 
 const Bar = () => {
   return (
-    <div>Bar</div>
+    <Box m="20px">
+        <Header title="Bar Chart" subtitle="Simple Bar Chart"/>
+        <Box height="75vh">
+            <BarChart/>
+        </Box>
+    </Box>
   )
 }
 
-export default Bar
+export default Bar;
