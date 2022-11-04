17b4c9ae6e799eff590c99b8752b4c472433b22c
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-04 23:58:49 +0200

    setup(PieChart): import most important things

---
+++ src/components/PieChart.jsx
@@ -0,0 +1,16 @@
+import { ResponsivePie } from "@nivo/pie";
+import {tokens} from "../theme";
+import { useTheme } from "@mui/material";
+import {mockPieData as data} from "../data/mocData";
+
+
+
+const PieChart = () => {
+
+    
+  return (
+    <div>PieChart</div>
+  )
+}
+
+export default PieChart
