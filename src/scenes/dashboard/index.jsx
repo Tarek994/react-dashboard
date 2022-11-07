3d04560ae120db4952ef70e7e83b0b7cc27dd595
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-07 03:15:11 +0200

    import(DashBoard): import all components those we want to show in Dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,9 +1,26 @@
-import { Box } from "@mui/system";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/system";
+import {tokens} from "../../theme";
+import {mockTranactions } from "../../data/mocData";
 import Header from "../../components/Header";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Emai";
+import PointOfSaleIcon from "@mui/icons-material/DownloadOutlined";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/BarChart";
+import PieChart from "../../components/PieChart";
+import GeographyChart from"../../components/GeographyChart"
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
+
 
 
 
 const Dashboard = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box
