52f6a4748e12fc9029e888f189d29d4dc4397e70
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-08 23:30:21 +0200

    dashboard(board-setup):set the grid style for Dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -13,6 +13,7 @@ import PieChart from "../../components/PieChart";
 import GeographyChart from"../../components/GeographyChart"
 import StatBox from "../../components/StatBox";
 import ProgressCircle from "../../components/ProgressCircle";
+import { fontSize } from "@mui/system";
 
 
 const Dashboard = () => {
@@ -47,6 +48,39 @@ const Dashboard = () => {
               Download Reports
           </Button>
        </Box>
+
+       {/* GRID & CHARTS */}
+
+        <Box
+          display="grid"
+          gridTemplateColumns="repeat(12, 1fr)"
+          gridAutoRows="140px"
+          gap="20px"
+        >
+          {/* ROW 1 */}
+          <Box gridColumn="span 3"
+               backgroundColor={colors.primary[400]}
+               display="flex"
+               alignItems="center"
+               justifyContent="center"
+               >
+              <StatBox
+                title="12,361"
+                subtitle="Emails Sent"
+                progress="0.75"
+                increase="+14%"
+                icon={
+                  <EmailIcon
+                   sx={{color: colors.greenAccent[600],
+                     fontSize: "26px"}}
+                     
+                     />
+                }
+              />
+
+          </Box>
+
+        </Box>      
       </Box>
   )
 }
