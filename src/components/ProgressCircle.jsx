88b6e827aa07cf1a1ce17f82f82deb50c4fade4b
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-09 00:46:54 +0200

    ProgressCircle: fix bugs of ProgressCircle Component

--- src/components/ProgressCircle.jsx
+++ src/components/ProgressCircle.jsx
@@ -10,10 +10,10 @@ const ProgressCircle = ({ progress = "0.75", size = "40"}) => {
     return(
         <Box 
             sx={{
-            background: `radial-gredient(${colors.primary[400]} 55%, transparent 56%),
+            background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
                 conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
                 ${colors.greenAccent[500]}`,
-            borderRadius: "50px",
+            borderRadius: "50%",
             width: `${size}px`,
             height: `${size}px`,
             }}
