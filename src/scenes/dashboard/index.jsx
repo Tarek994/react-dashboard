a016933dfc0dd11f1196ce9664ed2ed18e99aca8
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-09 03:18:49 +0200

    set the Recent Transactions section

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import {tokens} from "../../theme";
-import {mockTranactions } from "../../data/mocData";
+import {mockTransactions } from "../../data/mocData";
 import Header from "../../components/Header";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 import EmailIcon from "@mui/icons-material/Email";
@@ -137,7 +137,96 @@ const Dashboard = () => {
            </Box>
 
            
+          {/* ROW 2 */}
+          
+          <Box 
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          >
+            <Box
+              mt="25px"
+              p="0 30px"
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              >
+                <Box>
+                  <Typography variant="h5" fontWeight="600" color={colors.gray[100]}>
+                    Revenue Genetated
+                  </Typography>
+                  <Typography variant="h3" fontWeight="500" color={colors.greenAccent[500]}>
+                    $59,342,32
+                  </Typography>
+                </Box>
+                <Box>
+                 <DownloadOutlinedIcon
+                 sx={{ fontSize: "26px", color:colors.greenAccent[500] }}
+                 />    
+                </Box>
+            </Box>
+            <Box height="250px" ml="-20px">
+              <LineChart isDashboard={true} />
+            </Box>
+            </Box>
+                {/* TRANSACTIONS */}
 
+            <Box
+              gridColumn="span 4"
+              gridRow="span 2"
+              backgroundColor={colors.primary[400]}
+              overflow="auto"
+              >
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  borderBottom={`4px solid ${colors.primary[500]}`}
+                  colors={colors.gray[100]}
+                  p="15px"
+                >
+                  <Typography
+                   color={colors.gray[100]}
+                   variant="h5"
+                   fontWeight="600"
+                  >
+                    Recent Transactions
+                  </Typography>
+                </Box>
+                {mockTransactions.map((transaction, i)=>(
+                  <Box
+                  key={`${transaction.txId}- ${i}`}
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  borderBottom={`4px solid ${colors.primary[500]}`}
+                  p="15px"
+                  >
+                    <Box>
+                      <Typography
+                        color={colors.greenAccent[500]}
+                        variant="h5"
+                        fontWeight="600"
+                         >
+                          {transaction.txId}
+                      </Typography>
+                      <Typography
+                        color={colors.gray[100]}
+
+                         >
+                          {transaction.user}
+                      </Typography>
+                    </Box>
+
+                    <Box color={colors.gray[100]}>{transaction.date}</Box>
+
+                    <Box backgroundColor={colors.greenAccent[500]} p="5px 10px"
+                          borderRadius="4px"
+                    >{transaction.cost}</Box>
+                  </Box>
+                ))}
+            </Box>
+          
         </Box>      
       </Box>
   );
